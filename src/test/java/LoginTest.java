898435ccbe60685d2377dfb13084d429abb29d66
Rohan <42168488+Rohany7@users.noreply.github.com>
2023-04-01 03:57:45 +0530

    new method added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,4 +9,8 @@ public class LoginTest extends BaseTest{
         LoginPage loginPage = new LoginPage(driver);
         loginPage.Login(properties.getProperty("name"),properties.getProperty("pass"));
     }
+    @Test
+    public void TestMethod() {
+        System.out.println("In test method");
+    }
 }
